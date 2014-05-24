c9758762cf76619e7ea05ca9a8d707be3dadc3f0
Lwxiang <lwxiang1994@gmail.com>
2014-05-24 14:54:17 +0800

    init2

--- account/models.py
+++ account/models.py
@@ -8,10 +8,11 @@ import datetime
 class UserInfo(models.Model):
     user = models.OneToOneField(User)
     score = models.CharField(u'', max_length=5, default='0')
-    name = models.CharField(u'', max_length=10, default='unknown')
+    name = models.CharField(u'', max_length=10, default='佚名')
     birthday = models.DateField(u'', default=datetime.date(2000,1,1))
-    location = models.CharField(u'', max_length=30, default='the earth')
-    motto = models.CharField(u'', max_length=100, default='')
+    location = models.CharField(u'', max_length=30, default='未知')
+    motto = models.CharField(u'', max_length=100, default='我很懒，什么都不想写')
+    src_url = models.URLField(u'', max_length=30)
     
     
     
